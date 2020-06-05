adf3117fb6c06e70b2e23769b81efe23829bd611
Krystyna Łosieczka <krysia@agabox.net>
2020-06-05 23:39:29 +0200

    Updates

---
+++ precache-manifest.a3a9ab6639df4f16439744496bc76b03.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e9e9a7e2b92bc0424eb44e175088a2f",
+    "url": "/Peanut-Butter-Love/index.html"
+  },
+  {
+    "revision": "4c3175900ba6cde6093d",
+    "url": "/Peanut-Butter-Love/static/css/main.0de74216.chunk.css"
+  },
+  {
+    "revision": "91512261dd0fb8a6d869",
+    "url": "/Peanut-Butter-Love/static/js/2.ca48a5d1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Peanut-Butter-Love/static/js/2.ca48a5d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c3175900ba6cde6093d",
+    "url": "/Peanut-Butter-Love/static/js/main.f92c72fc.chunk.js"
+  },
+  {
+    "revision": "8511750fb30cb1547dbf",
+    "url": "/Peanut-Butter-Love/static/js/runtime-main.fab6b135.js"
+  },
+  {
+    "revision": "7aae589dfd42824e1b49ac0beb54327c",
+    "url": "/Peanut-Butter-Love/static/media/DogEatingPB.7aae589d.jpeg"
+  },
+  {
+    "revision": "c0e9fb943591dce48a1bcb7fe96919e8",
+    "url": "/Peanut-Butter-Love/static/media/Factory.c0e9fb94.jpg"
+  },
+  {
+    "revision": "f845a85b251af876d0c8b0a62204f680",
+    "url": "/Peanut-Butter-Love/static/media/PB&J.f845a85b.jpg"
+  },
+  {
+    "revision": "bd39c983ba29b4d07f873d2e70a27549",
+    "url": "/Peanut-Butter-Love/static/media/PBLlogoWhite.bd39c983.svg"
+  },
+  {
+    "revision": "0115b31bf436c8c0d48e3a74ea828c40",
+    "url": "/Peanut-Butter-Love/static/media/PBinJar.0115b31b.jpg"
+  },
+  {
+    "revision": "bf2272aa46df7367b7eb831ae2a84507",
+    "url": "/Peanut-Butter-Love/static/media/PBwithSpoon.bf2272aa.jpg"
+  }
+]);
